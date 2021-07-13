2064103550ed3d3d265fa0f5b24a7209b7c962c4
nindanaoto <dirty.knife603@gmail.com>
2021-07-14 01:02:21 +0900

    added annihilate

--- TFHElvl22.py
+++ TFHElvl22.py
@@ -25,6 +25,16 @@ class lvl1param:
     ε = 1/(2*(Bg**l))
     β = Bg/2
 
+class Annihilatelvl1param:
+    nbit = 10
+    n = 2**nbit
+    l = 12
+    Bgbit = 2
+    Bg = 2**Bgbit
+    α = 2**-25
+    ε = 1/(2*(Bg**l))
+    β = Bg/2
+
 class lvl2param:
     nbit = 11
     n = 2**nbit
@@ -56,15 +66,21 @@ def cmuxnoisecalc(P,α):
     # return 2*P.l*P.n*(P.β**2)*(α**2)+1/12*(1+P.n)*((2*P.ε)**2)
     return 2*P.l*P.n*(P.β**2)*(α**2)+(1+P.n)*(P.ε**2)
 
+def externalproduct(P):
+    return 2*P.l*P.n*(P.β**2)*(P.α**2)+(1+P.n)*P.n*(P.ε**2)
+
+def exprivkscalc(domainP,targetP,privksP):
+    return 1/12*(2**(-2*(privksP.basebit*privksP.t))) + externalproduct(targetP)
+
 def brnoisecalc(lowP,highP):
     return lowP.n*cmuxnoisecalc(highP,highP.α)
 
 # https://tches.iacr.org/index.php/TCHES/article/view/8793
-def iknoisecalc(lowP,highP,funcP):
-    return 1/12*highP.n*(2**(-2*(funcP.basebit*funcP.t)))+funcP.t*highP.n*(lowP.α**2)
+def iknoisecalc(domainP,targetP,funcP):
+    return 1/12*domainP.n*(2**(-2*(funcP.basebit*funcP.t)))+funcP.t*domainP.n*(targetP.α**2)
 
 def gbnoisecalc(lowP,highP,funcP):
-    return brnoisecalc(lowP,highP)+iknoisecalc(lowP,highP,funcP)
+    return brnoisecalc(lowP,highP)+iknoisecalc(highP,lowP,funcP)
 
 # Processor parameter
 
@@ -73,7 +89,10 @@ RAMaddress = 9
 RAMwordbit = 8
 
 print("TFHE IK noise")
-print(iknoisecalc(lvl0param,lvl1param,lvl10param))
+print(iknoisecalc(lvl1param,lvl0param,lvl10param))
+
+print("BR noise")
+print(brnoisecalc(lvl0param,lvl1param))
 
 print("TFHE Gate Bootstrapping Noise")
 
@@ -83,6 +102,19 @@ print(gbnoise)
 
 print("TFHE GB error prob")
 print(erfc(1/(16*np.sqrt(2*gbnoise))))
+
+def annihilatecalc(P):
+    return annihilaterecursive(P,P.nbit)
+
+def annihilaterecursive(P,nbit):
+    if(nbit==0):
+        return 0
+    else:
+        return 2*annihilaterecursive(P,nbit-1)+P.l*P.n*(P.β**2)*(P.α**2)+P.n*(P.ε**2)
+
+print("Annihilate")
+print(annihilatecalc(Annihilatelvl1param))
+# print(annihilatecalc(lvl2param))
 
 # https://tches.iacr.org/index.php/TCHES/article/view/8793
 def privksnoisecalc(domainP,targetP,privksP):
@@ -94,13 +126,18 @@ def cbnoisecalc(domainP,middleP,targetP,privksP):
 cbnoise = cbnoisecalc(lvl0param,lvl2param,lvl2param,lvl22param)
 
 print("TFHE lvl20 iks noise")
-print(iknoisecalc(lvl0param,lvl2param,lvl20param))
+print(iknoisecalc(lvl2param,lvl0param,lvl20param))
+
+print("TFHE lvl22 privks noise")
+print(privksnoisecalc(lvl2param,lvl2param,lvl22param))
+print(externalproduct(lvl2param))
+print(exprivkscalc(lvl0param,lvl2param,lvl22param))
 
 print("TFHE Circuit Bootstrapping lvl22 Noise")
 print(cbnoise)
 
 def romnoisecalc(addressP,dataP,middleP,ikP,privksP,ROMaddress):
-    return dataP.α+ROMaddress*cmuxnoisecalc(dataP,cbnoisecalc(addressP,middleP,dataP,privksP))+iknoisecalc(addressP,dataP,ikP)
+    return dataP.α+ROMaddress*cmuxnoisecalc(dataP,cbnoisecalc(addressP,middleP,dataP,privksP))+iknoisecalc(dataP,addressP,ikP)
 
 print("TFHE ROM CMUX noise")
 romnoise = romnoisecalc(lvl0param,lvl2param,lvl2param,lvl20param,lvl22param,ROMaddress)
@@ -110,7 +147,7 @@ print("TFHE ROM error prob")
 print(erfc(1/(16*np.sqrt(2*romnoise))))
 
 def ramnoisecalc(addressP,dataP,middleP,ikP,privksP,RAMaddress):
-    return brnoisecalc(addressP,dataP)+RAMaddress*cmuxnoisecalc(dataP,cbnoisecalc(addressP,middleP,dataP,privksP))+iknoisecalc(addressP,dataP,ikP)
+    return brnoisecalc(addressP,dataP)+RAMaddress*cmuxnoisecalc(dataP,cbnoisecalc(addressP,middleP,dataP,privksP))+iknoisecalc(dataP,addressP,ikP)
 
 print("RAM Read Noise")
 rnoise = ramnoisecalc(lvl0param,lvl2param,lvl2param,lvl20param,lvl22param,RAMaddress)
