3d202cd8e20c7316fe6bfbcd1e5ed6adf0ea7835
nindanaoto <matsuoka.kotaro@gmail.com>
2025-05-02 02:03:08 +0000

    bump lattice-estimator

--- python/newTFHE.py
+++ python/newTFHE.py
@@ -4,56 +4,56 @@ estimator = importlib.import_module(".estimator","lattice-estimator")
 # param = estimator.lwe_parameters.LWEParameters(
 #     n=128*3,
 #     q=2 ** 8,
-#     Xs=estimator.nd.NoiseDistribution.UniformMod(2),
-#    #  Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=2 ** (16-10.5)),
-#     Xe=estimator.nd.NoiseDistribution.CenteredBinomial(24),
+#     Xs=estimator.nd.UniformMod(2),
+#    #  Xe=estimator.nd.DiscreteGaussian(stddev=2 ** (16-10.5)),
+#     Xe=estimator.nd.CenteredBinomial(24),
 #     # m = 384,
 #     tag="ShortLWE",
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #     n=636,
 #     q=2 ** 16,
-#     Xs=estimator.nd.NoiseDistribution.UniformMod(2),
-# #    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=3.2),
-#     # Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 16),
-#     Xe=estimator.nd.NoiseDistribution.CenteredBinomial(40),
+#     Xs=estimator.nd.UniformMod(2),
+# #    Xe=estimator.nd.DiscreteGaussian(stddev=3.2),
+#     # Xe=estimator.nd.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 16),
+#     Xe=estimator.nd.CenteredBinomial(40),
 #     tag="TFHE636-16bit",
 # )
 # param = estimator.schemes.LightSaber
 # param = estimator.lwe_parameters.LWEParameters(
 #     n=586,
 #     q=2 ** 32,
-#     Xs=estimator.nd.NoiseDistribution.UniformMod(2),
-#     Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 32),
+#     Xs=estimator.nd.UniformMod(2),
+#     Xe=estimator.nd.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 32),
 #     tag="TFHE586",
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #    n=636,
 #    q=2 ** 32,
-#    Xs=estimator.nd.NoiseDistribution.UniformMod(2),
-#    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 32),
+#    Xs=estimator.nd.UniformMod(2),
+#    Xe=estimator.nd.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 32),
 #    tag="TFHE636",
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #    n=640,
 #    q=2 ** 32,
-#    Xs=estimator.nd.NoiseDistribution.UniformMod(2),
-#    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 32),
+#    Xs=estimator.nd.UniformMod(2),
+#    Xe=estimator.nd.DiscreteGaussian(stddev=0.000_092_511_997_467_675_6 * 2 ** 32),
 #    tag="TFHE640",
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #     n=777,
 #     q=2 ** 32,
-#     Xs=estimator.nd.NoiseDistribution.UniformMod(2),
-#     Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=2 ** (32-17.6)),
+#     Xs=estimator.nd.UniformMod(2),
+#     Xe=estimator.nd.DiscreteGaussian(stddev=2 ** (32-17.6)),
 #     tag="TFHE586",
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #     n=760,
 #     q=2 ** 32,
-#     Xs=estimator.nd.NoiseDistribution.UniformMod(2),
-#     # Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 2 ** 7),
-#     Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 2 ** (32-17)),
+#     Xs=estimator.nd.UniformMod(2),
+#     # Xe=estimator.nd.DiscreteGaussian(stddev= 2 ** 7),
+#     Xe=estimator.nd.DiscreteGaussian(stddev= 2 ** (32-17)),
 #     tag="TFHE760",
 # )
 param = estimator.lwe_parameters.LWEParameters(
@@ -62,46 +62,46 @@ param = estimator.lwe_parameters.LWEParameters(
    #  q= 3**4*2**16+1,
     q=2 ** 32,
    #  q=2 ** 25,
-    Xs=estimator.nd.NoiseDistribution.UniformMod(3),
-    # Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 2 ** 7),
-    # Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 4.2),
-   #  Xe=estimator.nd.NoiseDistribution.CenteredBinomial(2),
-    # Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 2*4/4),
-    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 0.000_000_034_233_878_701_836_9 * 2 ** 32),
+    Xs=estimator.nd.UniformMod(3),
+    # Xe=estimator.nd.DiscreteGaussian(stddev= 2 ** 7),
+    # Xe=estimator.nd.DiscreteGaussian(stddev= 4.2),
+   #  Xe=estimator.nd.CenteredBinomial(2),
+    # Xe=estimator.nd.DiscreteGaussian(stddev= 2*4/4),
+    Xe=estimator.nd.DiscreteGaussian(stddev= 0.000_000_034_233_878_701_836_9 * 2 ** 32),
     tag="TFHE1024",
 )
 # param = estimator.lwe_parameters.LWEParameters(
 #     n=768,
 #     q= (2 ** 16 + 1)*(2 **8 +1),
-#     Xs=estimator.nd.NoiseDistribution.UniformMod(3),
-#     # Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 2 ** 7),
-#     Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev= 8),
+#     Xs=estimator.nd.UniformMod(3),
+#     # Xe=estimator.nd.DiscreteGaussian(stddev= 2 ** 7),
+#     Xe=estimator.nd.DiscreteGaussian(stddev= 8),
 #     tag="TFHE1024",
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #    n=256*5,
 #    q=2 ** 64,
-#    Xs=estimator.nd.NoiseDistribution.UniformMod(3),
-# #    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=0.0000000000034525330484572114*2**64),
-#    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=2**-30*2**(64)),
+#    Xs=estimator.nd.UniformMod(3),
+# #    Xe=estimator.nd.DiscreteGaussian(stddev=0.0000000000034525330484572114*2**64),
+#    Xe=estimator.nd.DiscreteGaussian(stddev=2**-30*2**(64)),
 #    tag="TFHE1280"
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #    n=512*3,
 #    q=2 ** 64,
 #    # q=2 ** 48,
-#    Xs=estimator.nd.NoiseDistribution.UniformMod(3),
-#    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=2**(64-38)),
-#    # Xe=estimator.nd.NoiseDistribution.CenteredBinomial(3),
+#    Xs=estimator.nd.UniformMod(3),
+#    Xe=estimator.nd.DiscreteGaussian(stddev=2**(64-38)),
+#    # Xe=estimator.nd.CenteredBinomial(3),
 #    tag="TFHE1536",
 # )
 # param = estimator.lwe_parameters.LWEParameters(
 #    n=2048,
 # #    q=2 ** 64,
 #    q=2 ** 48,
-#    Xs=estimator.nd.NoiseDistribution.UniformMod(3),
-# #    Xe=estimator.nd.NoiseDistribution.DiscreteGaussian(stddev=2**(64-47)),
-#    Xe=estimator.nd.NoiseDistribution.CenteredBinomial(3),
+#    Xs=estimator.nd.UniformMod(3),
+# #    Xe=estimator.nd.DiscreteGaussian(stddev=2**(64-47)),
+#    Xe=estimator.nd.CenteredBinomial(3),
 #    tag="TFHE2048",
 # )
 print(param.n)
